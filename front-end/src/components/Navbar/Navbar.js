2b4d355462309de8b35c5b50654e7fac84befca5
Ali Ansari <a.ansari3103@gmail.com>
2024-01-19 16:06:04 +0330

    Updating navbar design

--- front-end/src/components/Navbar/Navbar.js
+++ front-end/src/components/Navbar/Navbar.js
@@ -12,7 +12,22 @@ const Navbar = () => {
         <hr className={classes.hr} />
         <img src={user} className={classes.user} alt="icon" />
       </div>
-      <img src={brands} className={classes.brands} alt="icon" />
+      <div className={classes.dropdown}>
+        <p className={classes.brand_text}>برند‌ها</p>
+        <img src={brands} className={classes.brands} alt="icon" />
+        <div className={classes.dropdown_content}>
+          <div>
+            <p>Ray Ban</p>
+            <p>Carrera</p>
+            <p>Oakley</p>
+          </div>
+          <div>
+            <p>Persol</p>
+            <p>Polaried</p>
+            <p>AliExpress</p>
+          </div>
+        </div>
+      </div>
       <img src={search} className={classes.search} alt="icon" />
     </div>
   );
