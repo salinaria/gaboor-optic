439b60d02bbd6102635950de9d7aa909799c7ad4
Ali Ansari <a.ansari3103@gmail.com>
2024-01-20 12:52:32 +0330

    Finishing dropdown brands

--- front-end/src/components/Navbar/Navbar.js
+++ front-end/src/components/Navbar/Navbar.js
@@ -9,26 +9,44 @@ const Navbar = () => {
     <div className={classes.navbar}>
       <div className={classes.left}>
         <img src={basket} className={classes.basket} alt="icon" />
-        <hr className={classes.hr} />
+        <div className={classes.hr} />
         <img src={user} className={classes.user} alt="icon" />
       </div>
       <div className={classes.dropdown}>
-        <p className={classes.brand_text}>برند‌ها</p>
-        <img src={brands} className={classes.brands} alt="icon" />
+        <div className={classes.brbutton}>
+          <p className={classes.brand_text}>برند‌ها</p>
+          <img src={brands} className={classes.brands} alt="icon" />
+        </div>
+        <div className={classes.brline} />
         <div className={classes.dropdown_content}>
-          <div>
-            <p>Ray Ban</p>
-            <p>Carrera</p>
-            <p>Oakley</p>
+          <div className={classes.first_col}>
+            <a href="#" className={classes.col}>
+              Ray Ban
+            </a>
+            <a href="#" className={classes.col}>
+              Correra
+            </a>
+            <a href="#" className={classes.col}>
+              Oakley
+            </a>
           </div>
-          <div>
-            <p>Persol</p>
-            <p>Polaried</p>
-            <p>AliExpress</p>
+          <div className={classes.sec_col}>
+            <a href="#" className={classes.col}>
+              Persol
+            </a>
+            <a href="#" className={classes.col}>
+              Polaried
+            </a>
+            <a href="#" className={classes.col}>
+              AliExpress
+            </a>
           </div>
         </div>
       </div>
-      <img src={search} className={classes.search} alt="icon" />
+      <div className={classes.right}>
+        <h1 className={classes.homepage}>GaboorOptic</h1>
+        <img src={search} className={classes.search} alt="icon" />
+      </div>
     </div>
   );
 };
