f8bbd521fe815358432395109d2349fe5b72f9d7
Ali Ansari <a.ansari3103@gmail.com>
2024-01-27 00:11:37 +0330

    Add next page to the newest

--- front-end/src/components/Home/Home.js
+++ front-end/src/components/Home/Home.js
@@ -2,13 +2,14 @@ import Navbar from "../Navbar/Navbar";
 import classes from "../Home/Home.module.css";
 import GlassMini from "../GlassMini/GlassMini";
 import glass1 from "../../assets/glass1.png";
+import glass2 from "../../assets/glass2.png";
+import glass3 from "../../assets/glass3.png";
+import next from "../../assets/next.svg";
 
 const Home = () => {
   return (
     <div>
       <Navbar />
-
-      
       <h1 className={classes.new_text}>جدیدترین عینک‌ها</h1>;
       <div className={classes.line}></div>
       <div className={classes.newest}>
@@ -19,17 +20,21 @@ const Home = () => {
           price="۱.۴۰۰"
         />
         <GlassMini
-          image={glass1}
+          image={glass2}
           name="Aviator Classic"
           brand="Ray Ban"
           price="۱.۴۰۰"
         />
         <GlassMini
-          image={glass1}
+          image={glass3}
           name="Aviator Classic"
           brand="Ray Ban"
           price="۱.۴۰۰"
         />
+        <a href="/trends" className={classes.next}>
+          <p className={classes.next_text}>مشاهده همه</p>
+          <img className={classes.next_img} src={next} alt="trends" />
+        </a>
       </div>
     </div>
   );
