474b3d4be9f2742b256dd9a17c99873cbc0cbd1d
Ali Ansari <a.ansari3103@gmail.com>
2024-02-16 10:22:04 +0330

    Starting to design wide glass delete

--- front-end/src/components/WideGlass/WideGlass.js
+++ front-end/src/components/WideGlass/WideGlass.js
@@ -1,12 +1,20 @@
 import classes from "../WideGlass/WideGlass.module.css";
+import deleteImage from "../../assets/delete.svg";
 
 const WideGlass = (props) => {
   return (
     <div className={classes.GlassMini}>
-      <a href={'http://localhost:3000/glass/'+props.array.sku_id} className={classes.link}>
+      <a
+        href={"http://localhost:3000/glass/" + props.array.sku_id}
+        className={classes.link}
+      >
         <img
           id="image"
-          src={"http://127.0.0.1:8000/store/store/images/" + props.array.sku_id + '.jpg'}
+          src={
+            "http://127.0.0.1:8000/store/store/images/" +
+            props.array.sku_id +
+            ".jpg"
+          }
           className={classes.image}
           alt="glassImage"
         />
@@ -15,6 +23,10 @@ const WideGlass = (props) => {
         <h1>{props.array.name.substring(0, 70)}</h1>
         <h1>{props.array.brand}</h1>
         <h1>{props.array.price}00</h1>
+        <div className={classes.delete}>
+          <p>حذف</p>
+          <img src={deleteImage} alt="delete" />
+        </div>
       </div>
     </div>
   );
