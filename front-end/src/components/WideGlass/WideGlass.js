cead82643039018d54cdd46a9934859a2cbef4b7
Ali Ansari <a.ansari3103@gmail.com>
2024-02-17 22:26:21 +0330

    Handling delete from basket

--- front-end/src/components/WideGlass/WideGlass.js
+++ front-end/src/components/WideGlass/WideGlass.js
@@ -4,8 +4,8 @@ import axios from "axios";
 import React, { useState } from "react";
 
 const WideGlass = (props) => {
-  const [isRemoved, setisRemoved] = useState(false);
-
+  const [isRemoved, setisRemoved] = useState(true);
+  
   const currentUser = () => {
     return JSON.parse(localStorage.getItem("currentUser"));
   };
@@ -22,36 +22,40 @@ const WideGlass = (props) => {
       "http://127.0.0.1:8000/api/delbasket/" + String(props.array.id) + "/",
       requestOptions
     );
-    setRemoved(true);
+    setisRemoved(false);
   }
   return (
-    <div className={classes.GlassMini}>
-      <a
-        href={"http://localhost:3000/glass/" + props.array.sku_id}
-        className={classes.link}
-      >
-        <img
-          id="image"
-          src={
-            "http://127.0.0.1:8000/store/store/images/" +
-            props.array.sku_id +
-            ".jpg"
-          }
-          className={classes.image}
-          alt="glassImage"
-        />
-      </a>
-      <div className={classes.info}>
-        <h1>{props.array.name.substring(0, 70)}</h1>
-        <h1>{props.array.brand}</h1>
-        <h1>{props.array.price}00</h1>
-        {props.basket ? (
-          <button onClick={remWishlist} className={classes.delete}>
-            <p>حذف</p>
-            <img src={deleteImage} alt="delete" />
-          </button>
-        ) : null}
-      </div>
+    <div>
+      {isRemoved ? (
+        <div className={classes.GlassMini}>
+          <a
+            href={"http://localhost:3000/glass/" + props.array.sku_id}
+            className={classes.link}
+          >
+            <img
+              id="image"
+              src={
+                "http://127.0.0.1:8000/store/store/images/" +
+                props.array.sku_id +
+                ".jpg"
+              }
+              className={classes.image}
+              alt="glassImage"
+            />
+          </a>
+          <div className={classes.info}>
+            <h1>{props.array.name.substring(0, 70)}</h1>
+            <h1>{props.array.brand}</h1>
+            <h1>{props.array.price}00</h1>
+            {props.basket ? (
+              <button onClick={remWishlist} className={classes.delete}>
+                <p>حذف</p>
+                <img src={deleteImage} alt="delete" />
+              </button>
+            ) : null}
+          </div>
+        </div>
+      ) : null}
     </div>
   );
 };
