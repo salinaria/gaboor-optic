45c221811fb38b719b5622b54a08e13a5ae5c00f
Ali Ansari <a.ansari3103@gmail.com>
2024-02-18 12:33:23 +0330

    Designing UserPage

--- front-end/src/components/Userpage/UserPage.js
+++ front-end/src/components/Userpage/UserPage.js
@@ -1,11 +1,12 @@
-import Navbar from "../Navbar/Navbar";
-import classes from "../Userpage/UserPage.module.css";
-import hello from "../../assets/hello.svg";
-import next from "../../assets/next.svg";
-import axios from "axios";
 import React, { useState, useEffect } from "react";
+import classes from "./UserPage.module.css";
+import Navbar from "../Navbar/Navbar";
+import signout from "..//..//assets//Signout.svg";
+import profile from "..//..//assets//Profile2.svg";
+import email from "..//..//assets//Message.svg";
+import axios from "axios";
 
-const UserPage = () => {
+const UserPage = (props) => {
   const [Data, setData] = useState({
     username: "",
     email: "",
@@ -29,39 +30,29 @@ const UserPage = () => {
   useEffect(() => {
     const requestOptions = customHeader();
     axios
-      .get("http://127.0.0.1:8000/api/account", requestOptions)
+      .get("http://127.0.0.1:8000/api/account",requestOptions)
       .then((response) => setData(response.data[0]));
   }, []);
+
   return (
     <div>
       <Navbar />
-      <div className={classes.welcome}>
-        <img src={hello} alt="hello" />
-        <p>سلام</p>
-        <p>{Data.username}</p>
+      <div className={classes.bacy}></div>
+      <div className={classes.body}>
+        <div className={classes.btn}>
+          <img src={profile} className={classes.btnimg} alt="prof" />
+          <p className={classes.txt}>{Data.username}</p>
+        </div>
+        <div className={classes.btn}>
+          <img src={email} className={classes.btnimg} alt="email" />
+          <p className={classes.txt}>{Data.email}</p>
+        </div>
+        <button className={classes.btn} onClick={signoutf}>
+          <img className={classes.btnimg} src={signout} alt="signout" />
+          <p className={classes.btntxt}>Log out</p>
+        </button>
       </div>
-      <div className={classes.change}>
-        <img src={next} alt="next" />
-        <p>تغییر نام</p>
-      </div>
-      <div className={classes.change}>
-        <img src={next} alt="next" />
-        <p>تغییر ایمیل</p>
-      </div>
-      <div className={classes.change}>
-        <img src={next} alt="next" />
-        <p>تغییر رمز عبور</p>
-      </div>
-      <button onClick={signoutf} className={classes.change}>
-        <img src={next} alt="next" />
-        <p>خروج</p>
-      </button>
-      
-      <div className={classes.purchase}>
-        <h1 className={classes.pur_text}>خرید‌های شما</h1>
-        <div className={classes.line}></div>
-        <div className={classes.pur_container}></div>
-      </div>
+      <div className={classes.bacx}></div>
     </div>
   );
 };
